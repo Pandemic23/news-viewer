35bfad0fc5825253b83d598ad69e50aa391563e3
Pandemic23 <popofh12345@naver.com>
2024-05-13 17:47:37 +0900

    newList 썸네일 링크 부가설명 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import NewsList from './NewsList';
 
 const App = () => {
-  // API를 넘겨받을 state 선언
-  const [data, setData] = useState(null);
-
-  const onClick = async () => {
-    // axios 라이브러리로 apic call
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e03681cbdffd4e5bb4f591ac741b7b86',
-      );
-      // 응답 data state 저장
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={() => onClick()}>불러오기</button>
-      </div>
-      {/* JSON 문자열 뿌릴 영역 */}
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
